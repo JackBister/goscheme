b3afa1ea62776b3108846aa1c1009e2f0414d9f9
Jack Bister <jack.bisther@outlook.com>
2015-08-29 23:03:52 +0200

    Add empty? function for lists

--- std/lists.scm
+++ std/lists.scm
@@ -1,11 +1,13 @@
+(define empty? (lambda (li) (eqv? li (list))))
+
 ;Because map runs in order in this implementation, map and for-each are equivalent.
 (define for-each map)
 
-(define member (lambda (obj li) (if (eqv? li (list)) #f (if (equal? obj (car li)) li (member obj (cdr li))))))
+(define member (lambda (obj li) (if (empty? li (list)) #f (if (equal? obj (car li)) li (member obj (cdr li))))))
 
-(define memq (lambda (obj li) (if (eqv? li (list)) #f (if (eq? obj (car li)) li (memq obj (cdr li))))))
+(define memq (lambda (obj li) (if (empty? li) #f (if (eq? obj (car li)) li (memq obj (cdr li))))))
 
-(define memv (lambda (obj li) (if (eqv? li (list)) #f (if (eqv? obj (car li)) li (memv obj (cdr li))))))
+(define memv (lambda (obj li) (if (empty? li) #f (if (eqv? obj (car li)) li (memv obj (cdr li))))))
 
-(define reverse (lambda (li) (if (eqv? li (list)) (list) (append (reverse (cdr li)) (list (car li))))))
+(define reverse (lambda (li) (if (empty? li) (list) (append (reverse (cdr li)) (list (car li))))))
 
