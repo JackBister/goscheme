ee04e393c935285124cf0343e20d57f59c04987b
Jack Bister <jack.bisther@outlook.com>
2015-08-29 22:26:48 +0200

    Fix error messages and map placement of remainder func
    Since remainder was originally called modulo it was in the wrong place in the
    std env map and the error messages were referring to it as modulo.

--- lib/numbers.go
+++ lib/numbers.go
@@ -18,10 +18,10 @@ func modulo(e Environment, args ...Expr) Expr {
 
 func remainder(e Environment, args ...Expr) Expr {
 	if _, ok := args[0].(Number); !ok {
-		return Error{"modulo: Argument 1 is not a number"}
+		return Error{"remainder: Argument 1 is not a number"}
 	}
 	if _, ok := args[1].(Number); !ok {
-		return Error{"modulo: Argument 2 is not a number"}
+		return Error{"remainder: Argument 2 is not a number"}
 	}
 	a0n := float64(args[0].(Number))
 	a1n := float64(args[1].(Number))
