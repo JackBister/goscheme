8354fca876bef22165c78c01c513a642a727599b
Jack Bister <jack.bisther@outlook.com>
2015-08-29 22:49:42 +0200

    Add for-each function

--- std/lists.scm
+++ std/lists.scm
@@ -1,3 +1,6 @@
+;Because map runs in order in this implementation, map and for-each are equivalent.
+(define for-each map)
+
 (define member (lambda (obj li) (if (eqv? li (list)) #f (if (equal? obj (car li)) li (member obj (cdr li))))))
 
 (define memq (lambda (obj li) (if (eqv? li (list)) #f (if (eq? obj (car li)) li (memq obj (cdr li))))))
