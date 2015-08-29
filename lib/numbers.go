5942c6b28b75f43262e3f5e6faeea8677ceba6bf
Jack Bister <jack.bisther@outlook.com>
2015-08-29 22:20:59 +0200

    Use float mod function in remainder

--- lib/numbers.go
+++ lib/numbers.go
@@ -1,5 +1,9 @@
 package goscheme
 
+import (
+	"math"
+)
+
 func remainder(e Environment, args ...Expr) Expr {
 	if _, ok := args[0].(Number); !ok {
 		return Error{"modulo: Argument 1 is not a number"}
@@ -7,9 +11,8 @@ func remainder(e Environment, args ...Expr) Expr {
 	if _, ok := args[1].(Number); !ok {
 		return Error{"modulo: Argument 2 is not a number"}
 	}
-	//Yuck!
-	a0n := int64(float64(args[0].(Number)))
-	a1n := int64(float64(args[1].(Number)))
-	return Number(a0n % a1n)
+	a0n := float64(args[0].(Number))
+	a1n := float64(args[1].(Number))
+	return Number(math.Mod(a0n, a1n))
 }
 
